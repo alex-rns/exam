35e7fd72021ba1d3dab8544de90dcf9f91ef1d04
alex-rns <alexrns3@gmail.com>
2018-03-20 00:53:19 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './Home'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        .kJSDHIFLUQ.WEGH;ASD/G
+        <Home/>
+      </div>
+    );
+  }
+}
+
+export default App;
